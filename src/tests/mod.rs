44640101263619e22935058ac1c5fcaab1fc5510
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-12-14 08:16:41 -0800

    added new file

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,8 +1,10 @@
 
 #[cfg(test)]
-pub mod tests;
+pub mod main_tests;
 
 #[cfg(test)]
 pub mod stream_states_tests;
 #[cfg(test)]
 pub mod message_handler_tests;
+#[cfg(test)]
+pub mod io_handler_tests;
