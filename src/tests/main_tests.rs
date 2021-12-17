31b628cb016208c68c761d19321f635e0f2a1a62
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-12-17 12:49:27 -0800

    added connection test.

--- src/tests/main_tests.rs
+++ src/tests/main_tests.rs
@@ -12,3 +12,15 @@ fn can_make_socket_listener(){
     let listener = make_listener(String::from("localhost:5000"));
     drop(listener);
 }
+
+#[test]
+fn create_and_connect_to_listener() {
+    let listener = make_listener(String::from("localhost:5000"));
+
+    let join_handle = std::thread::spawn(move || {
+        let _outgoing = std::net::TcpStream::connect("localhost:5000").unwrap();
+    });
+    join_handle.join().unwrap();
+    drop(listener);
+}
+
