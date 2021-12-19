d2b5ea643d15212ee744d2b98aa529e0d18f82a9
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-12-18 18:34:36 -0800

    testing the ctrc_c setup function

--- src/tests/main_tests.rs
+++ src/tests/main_tests.rs
@@ -1,9 +1,16 @@
+use workctl::sync_flag;
 
 
 #[test]
 fn it_works() {
     let result = 2 + 2;
     assert_eq!(result, 4);
+}
+
+#[test]
+fn can_make_ctrl_c_handler() {
+    let (control_c_flag_tx, control_c_called_flag_rx) = sync_flag::new_syncflag(false);
+    crate::setup_control_c(control_c_flag_tx);
 }
 
 
