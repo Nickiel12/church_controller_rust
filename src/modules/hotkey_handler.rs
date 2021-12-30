bead4a5b99626e0cb6b6b320d3b03411738412a9
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-12-29 21:29:35 -0800

    simple send-update-on hotkey initiation

---
+++ src/modules/hotkey_handler.rs
@@ -0,0 +1,16 @@
+use super::stream_states::{state_update::StateUpdate, enums::SlideChange};
+
+pub fn create_keyboard_hooks(channel_tx: crossbeam_channel::Sender<String>) {
+    
+    let tx_1 = channel_tx.clone();
+    inputbot::KeybdKey::PageUpKey.bind(move || {
+        tx_1.send(StateUpdate::ChangeSlide(SlideChange::Next).to_json().to_string()).unwrap();
+    });
+
+    let tx_2 = channel_tx.clone();
+    inputbot::KeybdKey::PageDownKey.bind(move || {
+        tx_2.send(StateUpdate::ChangeSlide(SlideChange::Previous).to_json().to_string()).unwrap();
+    });
+    
+    inputbot::handle_input_events();
+}
