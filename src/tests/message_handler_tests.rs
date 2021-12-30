34316bacc1fe3502390a46efc7bcbb6d1a84719a
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-12-30 13:24:41 -0800

    added settings from other program

--- src/tests/message_handler_tests.rs
+++ src/tests/message_handler_tests.rs
@@ -1,10 +1,13 @@
-use crate::modules::{message_handler::{MessageHandler}, stream_states::{state_update::StateUpdate, stream_states_class::StreamState}};
+use crate::modules::{message_handler::{MessageHandler}, stream_states::{state_update::StateUpdate, stream_states_class::StreamState}, external_interface::Hotkeys};
 
 
 #[test]
 fn does_stream_state_implement_message_handler() {
+    let hotkeys = Hotkeys {
+        hotkeys: serde_json::Value::Null
+    };
     let mut state = StreamState::new();
     state.debug_mode = true;
-    state.handle_update(StateUpdate::ToggleComputerSoundOn(false));
+    state.handle_update(StateUpdate::ToggleComputerSoundOn(false), &hotkeys);
     assert_eq!(state.computer_sound_is_on, false);
 }
