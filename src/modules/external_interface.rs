6471efe7e5bf01cec878c90885eeb90d45123699
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-12-30 13:25:08 -0800

    added hotkeyclass to store shortcuts

--- src/modules/external_interface.rs
+++ src/modules/external_interface.rs
@@ -1,5 +1,8 @@
 use super::stream_states::{state_update::StateUpdate, enums::{SlideChange, SubScenes, Scenes}};
 
+const AHK_FILES_FOLDER: &str = "./src/ahk_files";
+pub const OPTIONS_PATH: &str = "./options.json";
+
 pub fn create_keyboard_hooks(channel_tx: crossbeam_channel::Sender<String>) {
     
     let tx_1 = channel_tx.clone();
@@ -15,26 +18,32 @@ pub fn create_keyboard_hooks(channel_tx: crossbeam_channel::Sender<String>) {
     inputbot::handle_input_events();
 }
 
-pub fn next_slide() {
-    todo!()
+pub struct Hotkeys {
+    pub hotkeys: serde_json::Value,
 }
 
-pub fn prev_slide() {
-    todo!()
-}
-
-pub fn toggle_stream_sound(value: bool) {
-    todo!()
-}
-
-pub fn toggle_computer_sound(value: bool) {
-    todo!()
-}
-
-pub fn toggle_media_play_pause(value: bool) {
-    todo!()
-}
-
-pub fn change_scene(scene: Scenes, sub_scene: Option<SubScenes>) {
-    todo!()
+impl Hotkeys {
+    pub fn next_slide(&self, ) {
+        todo!()
+    }
+    
+    pub fn prev_slide(&self, ) {
+        todo!()
+    }
+    
+    pub fn toggle_stream_sound(&self, value: bool) {
+        todo!()
+    }
+    
+    pub fn toggle_computer_sound(&self, value: bool) {
+        todo!()
+    }
+    
+    pub fn toggle_media_play_pause(&self, value: bool) {
+        todo!()
+    }
+    
+    pub fn change_scene(&self, scene: Scenes, sub_scene: Option<SubScenes>) {
+        todo!()
+    }
 }
